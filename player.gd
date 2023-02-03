56f12f96d0e0c3164ca3b71b0298d06d5a0671ec
Mylagos <Eddybo2001@gmail.com>
2023-02-03 22:42:19 +0100

    Smoothed move func

--- player.gd
+++ player.gd
@@ -10,17 +10,25 @@ extends KinematicBody2D
 func _ready():
 	pass # Replace with function body.
 
-var speed = 100
+var speed = 200
 var velocity = Vector2()
 
-func _max_accelerate(diagonalMove):
+func _max_accelerate():
 	if abs(velocity.x) > speed+1:
 		velocity.x = velocity.x/abs(velocity.x)*speed
 	if abs(velocity.y) > speed:
 		velocity.y = velocity.y/abs(velocity.y)*speed
 	
+func _stop_character():
+	if abs(velocity.x) < 10:
+		velocity.x = 0
+	if abs(velocity.y) < 10:
+		velocity.y = 0
 
 func _move(delta):
+	
+	_stop_character()
+	
 	var diagonalMove = 0
 	if Input.is_action_pressed("up"):
 		velocity += Vector2(0, -speed)
@@ -34,12 +42,14 @@ func _move(delta):
 	if Input.is_action_pressed("right"):
 		velocity += Vector2(speed, 0)
 		diagonalMove += 1
-	print(diagonalMove) 
-	_max_accelerate(diagonalMove)
+	
+	_max_accelerate()
+	
 	if diagonalMove == 2:
 		velocity /= 1.4
 	
-	velocity /= 1.1
+	#natural decceleration
+	velocity /= 1.2
 	
 	position = position + velocity * delta
 
